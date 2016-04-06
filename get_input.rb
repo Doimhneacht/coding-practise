f7db132464acfc4558a079de323e289a912b1463
Doimhneacht <hanna.rosul@gmail.com>
2016-04-06 19:46:03 +0300

    Add input validation and define a static method for prompt

--- get_input.rb
+++ get_input.rb
@@ -1,7 +1,20 @@
-print "Enter a natural number: "
-n = gets.chomp.to_i
+class String
+  def is_natural?
+    number = self.to_i
+    number > 0 && number.to_s == self
+  end
+end
 
-until n.integer?
-  print "Please make sure that your number is natural\nand try again: "
-  n = gets.chomp.to_i
+class Input
+  def self.get_n
+    print "Enter a natural number: "
+    n = gets.chomp
+
+    until n.is_natural?
+      print "Please make sure that your number is natural and try again: "
+      n = gets.chomp
+    end
+
+    n.to_i
+  end
 end
